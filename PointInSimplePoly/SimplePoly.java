596f61492bc96766bbf9b1627671bc4335c67333
2078910A <baitch1996@gmail.com>
2017-09-24 17:06:52 +0100

    TODO: Algorithm in SimplePoly.java file

--- PointInSimplePoly/SimplePoly.java
+++ PointInSimplePoly/SimplePoly.java
@@ -4,15 +4,23 @@ public class SimplePoly{
 
     private LinkedList<Edge> edges;
 	
-	public SimplePoly() {
-		this.edges = new LinkedList<Edge>();
-	}
+    public SimplePoly() {
+        this.edges = new LinkedList<Edge>();
+    }
 
-	public SimplePoly(LinkedList<Edge> e) {
-		edges = e;
-	}
+    public SimplePoly(LinkedList<Edge> e) {
+        edges = e;
+    }
 	
-	public LinkedList<Edge> getEdges() {return edges;}
-	public void addEdge(Edge ed){ edges.add(ed); }
+    public LinkedList<Edge> getEdges() {return edges;}
+    public void addEdge(Edge ed){ edges.add(ed); }
 	
+    //TODO: Write algorithm which checks every edge of Poly
+    //      and counts how many edges are intercepted by the
+    //      line x=x_0, where x_0 is the value of the xCoord 
+    //      of the point being checked. See Manber, Ch:8, p269
+    public boolean isPointInside(Point checkPoint) {
+        return true;
+    }
+
 }
